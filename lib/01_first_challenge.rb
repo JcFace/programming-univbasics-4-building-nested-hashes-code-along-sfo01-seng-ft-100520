5924915f761fe245a8cb9985caa9651ed8ae8661
Jesse Vaughn <jcv221@gmail.com>
2020-09-15 08:06:02 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -4,3 +4,15 @@ def first_challenge
     capulet: {}
   }
 end
+ montague: {
+     patriach: {name: "Lord Montague", 
+    age: "53"
+   },
+   capulet: {
+     patriach: {
+       name: "Lord Capulet",
+       age: "50"
+     }
+   }
+   }
+  }
