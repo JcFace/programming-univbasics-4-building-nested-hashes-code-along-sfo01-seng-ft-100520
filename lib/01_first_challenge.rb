4ff852151565f1e9603c461f0bb58d3c3d93de7a
Jesse Vaughn <jcv221@gmail.com>
2020-09-15 07:55:40 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,10 @@
-def first_challenge
-   epic_tragedy = {}
-end
+#def first_challenge
+  # epic_tragedy = {
+    # montague: {
+    # patriach: {name: "Lord Montague", 
+   #  age: "53"
+  # },
+   #matriach
+  # capulet: {}
+   #}
+#end
